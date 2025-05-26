3e38cb00f1c2524b8499393eae43106edc5efcbf
kennydev7 <gabrielcad28@gmail.com>
2025-05-25 21:21:03 -0300

    Add files via upload

---
+++ exercicio01.java
@@ -0,0 +1,20 @@
+public class exercicio01 {
+    public static void main(String[] args)
+    {
+        String perfil = "JOAO PEDRO";
+        System.out.println("Olá " + perfil);
+
+        String comprimento = "Tudo bem?";
+        System.out.println(comprimento);
+
+        String estudos = "anotação exemplo";
+        System.out.println(estudos);
+
+        String soma = "15";
+        System.out.println(soma);
+
+        String subtracao = "5";
+        System.out.println(subtracao);
+    }
+
+}
